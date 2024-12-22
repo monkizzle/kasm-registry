26ce2071e820faf79f90e21e317a1176ce017e0e
monkizzle <alex.quach@gmail.com>
2024-12-22 13:31:14 -0500

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Not A Cloud Technologies',
+    description: 'The official store for workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://monkizzle.github.io/kasm-registry/',
+    contactUrl: 'https://github.com/monkizzle/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
